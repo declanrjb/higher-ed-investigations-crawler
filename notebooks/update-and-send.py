3e5a1e9bd2426366e4591a03c10570c5e1c5e002
declanrjb <declanrjb@gmail.com>
2024-10-17 16:52:58 -0400

    latest data

---
+++ notebooks/update-and-send.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+import datetime
+from datetime import date
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+# In[ ]:
+
+
+load_dotenv('sendgrid.env')
+
+
+# In[11]:
+
+
+def data_from_row(row):
+    row_data = {}
+    for cell in row.select('td'):
+        row_data[cell['headers'][0]] = cell.text
+    return row_data
+
+def retrieve_page(page_index,params):
+    url = 'https://ocrcas.ed.gov/open-investigations?' + '&'.join([k + '=' + str(v) for k, v in zip(params.keys(),params.values())]) + f'&page={page_index}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text)
+    return soup
+
+def page_has_data(page):
+    return len(page.select('table')) > 0
+
+def scrape_page(page):
+    table = page.select('table')[0].select('tbody')[0]
+    df = pd.DataFrame([data_from_row(row) for row in table.select('tr')])
+    return df
+
+
+# In[12]:
+
+
+params = {
+    'field_ois_state':'All',
+    'field_ois_discrimination_statute':'All',
+    'field_ois_type_of_discrimination':'All',
+    'items_per_page':20,
+    'field_ois_institution':'',
+    'field_ois_institution_type':752,
+    'field_open_investigation_date_1':str(date.today() - datetime.timedelta(days=7)),
+    'field_open_investigation_date_2':str(date.today()),
+    'field_open_investigation_date':'',
+    'field_open_investigation_date_3':'',
+}
+
+
+# In[13]:
+
+
+tables = []
+for i in range(0,99999999):
+    page = retrieve_page(i,params)
+    if page_has_data(page):
+        tables.append(scrape_page(page))
+    else:
+        break
+
+
+# In[ ]:
+
+
+if len(tables) > 0:
+    df = pd.concat(tables)
+    df.columns = ['STATE','INST','INST_TYPE','DISCRIMINATION_TYPE','INVEST_START_DATES']
+
+    df = df[['STATE','INST','DISCRIMINATION_TYPE','INVEST_START_DATES']]
+    for col in ['STATE','INST','DISCRIMINATION_TYPE']:
+        df[col] = df[col].str.strip()
+
+    message = Mail(
+        from_email='declan@declanrjb.com',
+        to_emails='dbradley@chronicle.com',
+        subject=f'{len(df["INST"])} DoE investigations opened last week',
+        html_content=f'<p>Investigations spanned {len(df["INST"].unique())} institutions in {len(df["STATE"].unique())} states.</p><br>{df.to_html()}')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        sg.send(message)
+    except Exception as e:
+        print(e.message)
+
--- shell-scripts/send-weekly-update.sh
+++ shell-scripts/send-weekly-update.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 cd ../notebooks
 jupyter nbconvert --to python update-and-send.ipynb
 python3 update-and-send.py
-rm update-and-send.py
 cd ..
 git add *
 git commit -m 'latest data'
