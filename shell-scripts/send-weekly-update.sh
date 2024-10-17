778bb29de615f97414e1189d169d4569337635e2
declanrjb <declanrjb@gmail.com>
2024-10-17 16:17:03 -0400

    latest data

--- shell-scripts/send-weekly-update.sh
+++ shell-scripts/send-weekly-update.sh
@@ -1,4 +1,8 @@
 cd ../notebooks
 jupyter nbconvert --to python update-and-send.ipynb
 python3 update-and-send.py
-rm update-and-send.ipynb
+rm update-and-send.py
+cd ..
+git add *
+git commit -m 'latest data'
+git push origin main
