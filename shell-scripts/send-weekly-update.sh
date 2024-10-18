e7eee571186a0f1e1c4eb650747b999a000b7995
declanrjb <declanrjb@gmail.com>
2024-10-18 09:48:01 -0400

    latest data

--- shell-scripts/send-weekly-update.sh
+++ shell-scripts/send-weekly-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /Users/declanbradley/Documents/Work/Journalism/Chronicle/higher-ed-investigations-crawler/shell-scripts/
 cd ../notebooks
-update-and-send.ipynb -m jupyter nbconvert --to python 
+update-and-send.ipynb -m jupyter nbconvert --to python --output update-and-send.py
 python3 update-and-send.py
 cd ..
 git add *
