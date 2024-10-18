e73acb36b6cc32dcc73cfa3ffabf19dbb0120575
declanrjb <declanrjb@gmail.com>
2024-10-18 10:18:05 -0400

    latest data

--- shell-scripts/send-weekly-update.sh
+++ shell-scripts/send-weekly-update.sh
@@ -3,6 +3,7 @@ cd /Users/declanbradley/Documents/Work/Journalism/Chronicle/higher-ed-investigat
 cd ../notebooks
 jupytext --to py update-and-send.ipynb
 python3 update-and-send.py
+rm update-and-send.py
 cd ..
 git add *
 git commit -m 'latest data'
