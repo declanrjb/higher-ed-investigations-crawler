011b405adcb88b295882bb2e2d6e8bf12c2287b6
declanrjb <declanrjb@gmail.com>
2024-10-18 09:51:11 -0400

    latest data

--- shell-scripts/send-weekly-update.sh
+++ shell-scripts/send-weekly-update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /Users/declanbradley/Documents/Work/Journalism/Chronicle/higher-ed-investigations-crawler/shell-scripts/
 cd ../notebooks
+pip3 install jupytext
+pip3 install requests
 jupytext --to py update-and-send.ipynb
 python3 update-and-send.py
 cd ..
